cc48562d6de1b4eb2459c7b1be285b81857b3e89
Dynee <lucaspadden@gmail.com>
2020-10-04 19:04:13 -0400

    added .swp file to gitignore, created a canvas class to deal with all canvas related things i.e drawing, clearing the canvas ect.

--- game.py
+++ game.py
@@ -18,6 +18,25 @@ class Color(Enum):
     PURPLE = (191, 0, 255)
     PINK = (255, 0, 191)
 
+class Canvas():
+    def __init__(self, screen, width, height, pos, border=1):
+        self.screen = screen
+        self.width = width
+        self.height = height
+        self.border = border
+        self.pos = pos
+
+    def setup(self):
+        pygame.draw.rect(self.screen, Color.BLACK.value, [self.pos[0], self.pos[1], self.width, self.height], self.border)
+
+    def draw(self):
+        """ handles drawing on the canvas """
+
+    def clear(self):
+        """ clears the canvas of any drawings """
+
+
+
 def draw_colors(screen):
     """
     This will draw the colors on a screen that can be selected like in paint
@@ -37,23 +56,29 @@ def draw_colors(screen):
 
 screen.fill(Color.WHITE.value)
 colors = draw_colors(screen)
-selected_color = pygame.draw.rect(screen, Color.BLACK.value, [20, 300, 50, 50])
+selected_color = pygame.draw.rect(screen, Color.BLACK.value, [20, 50, 50, 50])
+# acts as the canvas that can be drawn on
+canvas = Canvas(screen, 465, 565, [20, 110])
+canvas.setup()
 
 while True: 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
         if event.type == pygame.MOUSEBUTTONDOWN:
+            drag = True
             x, y = event.pos
             for color in colors:
                 if color['rect'].collidepoint(x, y):
                     selected_color = color['color']
-                    selected_color_rect = pygame.draw.rect(screen, Color[selected_color].value, [20, 300, 50, 50])
-    print(selected_color)
+                    selected_color_rect = pygame.draw.rect(screen, Color[selected_color].value, [20, 50, 50, 50])
+                    # draw a line between start pos and end pos
+
+        if event.type == pygame.MOUSEBUTTONUP:
+            drag = False
     # flip updates the screen with what you draw
 
     # draw the selected color rect
-    # selected_color_rect = pygame.draw.rect(screen, Color.BLACK.value, [20, 300, 50, 50])
     pygame.display.flip()
 
 pygame.quit()
