1fc0f6860a86622270d50bb9b75e7b1a06d64867
Dynee <lucaspadden@gmail.com>
2020-10-04 15:14:39 -0400

    positioned color rectangles in a line

--- game.py
+++ game.py
@@ -18,11 +18,29 @@ class Color(Enum):
     PURPLE = (191, 0, 255)
     PINK = (255, 0, 191)
 
+def draw_colors(screen):
+    """
+    This will draw the colors on a screen that can be selected like in paint
+    draw a 20, 20 rect for each color in the colors enum
+
+    """
+    top, left, width, height = 20, 20, 20, 20
+    for color in Color:
+        pygame.draw.rect(screen, color.value, [top, left, width, height])
+        # next rect should be 20 to the right
+        top += 20
+
+
+
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
    
     screen.fill(Color.WHITE.value)
+
+    # draw colors box
+    draw_colors(screen)
 
     # flip updates the screen with what you draw
     pygame.display.flip()
